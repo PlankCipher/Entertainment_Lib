d5c20bb4583e788d01f98f6de85a731dd2b74b8b
PlankCipher <3abqarino3@gmail.com>
2020-03-27 19:54:17 -0700

    Add project files.

---
+++ Entertainment_Lib/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entertainment_Lib.Startup))]
+namespace Entertainment_Lib
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
