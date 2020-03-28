402b348f0b3ef3a314e5c99de5fb86e44ac14680
PlankCipher <3abqarino3@gmail.com>
2020-03-27 20:03:17 -0700

    Remove some ASP.NET MVC default files and settings.

--- Entertainment_Lib/App_Start/BundleConfig.cs
+++ Entertainment_Lib/App_Start/BundleConfig.cs
@@ -19,12 +19,8 @@ namespace Entertainment_Lib
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/general.css"));
         }
     }
 }
