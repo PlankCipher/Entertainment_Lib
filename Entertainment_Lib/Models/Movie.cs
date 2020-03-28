a5bcc71022f64b634ded8db4fdbe31553c3047d3
PlankCipher <3abqarino3@gmail.com>
2020-03-27 20:57:06 -0700

    Add Movie and TVProgram models.

---
+++ Entertainment_Lib/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entertainment_Lib.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ApplicationUser Owner { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
