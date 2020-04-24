c006c0e5ec0c43924c34d898b46708821b9f3543
PlankCipher <3abqarino3@gmail.com>
2020-04-24 09:11:30 -0700

    List latest movies and tvprograms in home page.

--- Entertainment_Lib/Controllers/HomeController.cs
+++ Entertainment_Lib/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Entertainment_Lib.Models;
+using Entertainment_Lib.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,27 @@ namespace Entertainment_Lib.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context { get; set; }
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            HomeViewModel model = new HomeViewModel()
+            {
+                latestMovies = _context.Movies.Take(5).ToList(),
+                latestTVPrograms = _context.TVPrograms.Take(5).ToList()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ Entertainment_Lib/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Entertainment_Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entertainment_Lib.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Movie> latestMovies { get; set; }
+        public IEnumerable<TVProgram> latestTVPrograms { get; set; }
+    }
+}
