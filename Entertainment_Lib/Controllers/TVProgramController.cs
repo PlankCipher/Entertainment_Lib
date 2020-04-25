264e4276738fac4bec5b2a476ef1c366d8ef5911
PlankCipher <3abqarino3@gmail.com>
2020-04-24 17:02:56 -0700

    List all TV programs in the view of index action of TVProgram controller.

--- Entertainment_Lib/Controllers/TVProgramController.cs
+++ Entertainment_Lib/Controllers/TVProgramController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entertainment_Lib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,24 @@ namespace Entertainment_Lib.Controllers
 {
     public class TVProgramController : Controller
     {
+        private ApplicationDbContext _context { get; set; }
+
+        public TVProgramController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: TVProgram
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<TVProgram> tvprograms = _context.TVPrograms.ToList();
+
+            return View(tvprograms);
         }
     }
 }
